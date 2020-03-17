6bb77748ff1809bffcce162a2c260e1032811707
Devin Wallace <devinwallace@protonmail.com>
2020-03-17 00:53:41 -0500

    rm some files

--- emaill.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$name = echo $_POST['name'];
-$email = echo $_POST['email'];
-$msg = echo $_POST['message'];
-$sub = echo $_POST['subject'];
-
-mail("devinwallace@protonmail.com",$sub,$msg);
-?>
